df30c2863c48ff211738c76a77dc0237b9e47647
gustaveH <ghabiman@gmail.com>
2020-07-29 19:40:01 -0400

    added TouList and Tour folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Nabvar';
 import './App.scss';
+import TourList from './components/TourList';
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <TourList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TourList.scss';
+
+class TourList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Tourlist</h1>
+      </div>
+    );
+  }
+}
+export default TourList;
