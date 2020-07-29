2b47bf5b54dbeeb855a5fc6d59d9c6c76d55a12a
gustaveH <ghabiman@gmail.com>
2020-07-29 19:32:23 -0400

    styled Navbar

--- src/components/Navbar/Nabvar.js
+++ src/components/Navbar/Nabvar.js
@@ -6,6 +6,23 @@ export default function Nabvar() {
   return (
     <nav className='navbar'>
       <img src={logo} alt='travel logo' />
+      <ul className='nav-links'>
+        <li>
+          <a href='/' className='nav-link'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='/' className='nav-link'>
+            About
+          </a>
+        </li>
+        <li>
+          <a href='/' className='nav-link active'>
+            Tours
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
